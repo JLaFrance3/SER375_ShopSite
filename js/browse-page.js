0fabe00d08ba6ead7f1de88d04f1f13ebf76e60c
jlafrance3 <jlafranceiii@proton.me>
2025-02-27 17:38:16 -0500

    Added product cards to browse page

---
+++ js/browse-page.js
@@ -0,0 +1,60 @@
+//load products for product grid on browse page
+async function loadItems() {
+    const numItems = document.getElementById("num-items");
+    const productGrid = document.getElementById("product-grid");
+
+    // Get JSON
+    const response = await fetch('http://67.205.143.29:3000/products');
+    const products = await response.json();
+
+    let productCounter = 0;
+
+    products.forEach((product, index) => {
+        productCounter++;
+
+        //Create star rating
+        let stars = '';
+        for (let i = 0; i < 5; i++) {
+            if (i < Math.floor(product.ratingAverage)) {
+                stars += createStar(index * 10 + i);
+            }
+            else {
+                if (i >= Math.ceil(product.ratingAverage)) {
+                    stars += createStar(index * 10 + i, 0)
+                }
+                else {
+                    stars += createStar(index * 10 + i, (product.ratingAverage % 1) * 100)
+                }
+            }
+        }
+
+        //Calculate price after discount. Set to 0 if discounted price less than 0.
+        const price = (product.price - product.discount) > 0 ? (product.price - product.discount) : 0
+
+        //Create product card
+        productGrid.innerHTML += `
+            <div class="product-card">
+                <div class="product-image-container">
+                    <img src="${product.images[0]}" alt="Picture of ${product.description}">
+                </div>
+                <div class="product-details">
+                    <h4>${product.name}</h4>
+                    <div class="star-rating">
+                        ${stars}
+                    </div>
+                    <p>
+                        ${price.toLocaleString('en-US', {
+                            style: 'currency',
+                            currency: 'USD'
+                        })}
+                    </p>
+                </div>
+            </div>
+        `;
+    });
+
+    //Update numItems on page
+    numItems.innerText = productCounter;
+}
+
+loadItems();
